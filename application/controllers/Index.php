a5301756c588a0234edfe08da35004794136bb6b
xiebaichuan <xbc@bronet.cn>
2020-05-23 20:46:02 +0800

    ok

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -5,6 +5,7 @@
  * @desc 默认控制器
  * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
  */
+use Illuminate\Database\Capsule\Manager as DB;
 class IndexController extends BaseController {
     /**
      * 默认初始化方法，如果不需要，可以删除掉这个方法
@@ -25,13 +26,9 @@ class IndexController extends BaseController {
 	}
 
     public function testAction() {
-        $res = \Illuminate\Support\Facades\DB::table('users')->select('name');
-        print_r($res);die;
         $name = $this->getRequest()->getPost('name', '');
         $pwd = $this->getRequest()->getPost('pwd', '');
-
-        $list = UserModel::where([['name', '=', $name], ['pwd', '=', $pwd]])->first();
-        $this->getResponse()->setBody(json_encode($list));
+        $this->getResponse()->setBody(json_encode($pwd));
         return true;
     }
 }
